56634657e788f21044ba6ef06a05ea51e8904e48
Zahniar Adirahman <zahniar88@gmail.com>
2020-07-25 09:59:11 +0700

    Bug Fixed and install dotenv in local

--- Main.py
+++ Main.py
@@ -14,11 +14,13 @@ class Main(App, Configure, Command, HttpConfig, HttpsConfig, HostConfig):
         self.OS = os
         # .env configure
         self.CURRENT_DIR = os.path.dirname(os.path.realpath(__file__)) + "/"
-        self.DEFAULT_DIR = os.getenv("DEFAULT_DIR") + "/"
-        self.DEFAULT_USER = os.getenv("DEFAULT_USER")
-        self.DEFAULT_SSL_LOCATION = os.getenv("DEFAULT_SSL_LOCATION") + "/"
-        
         self.ENV_FILE = os.path.exists(self.CURRENT_DIR + ".env")
+        
+        if self.ENV_FILE:
+            self.DEFAULT_DIR = os.getenv("DEFAULT_DIR") + "/"
+            self.DEFAULT_USER = os.getenv("DEFAULT_USER")
+            self.DEFAULT_SSL_LOCATION = os.getenv("DEFAULT_SSL_LOCATION") + "/"
+        
         self.AppRunnning(self)
         
 # inisialisasi class main
